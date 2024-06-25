2e0ef44095c92a826f98862f73f57006338d0058
Jake Moore <jakealexandermoore@icloud.com>
2024-06-25 15:39:19 -0700

    Initial Commit - Plugin Set Up

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+plugins {
+    id("java")
+    id("java-library")
+    id("io.github.goooler.shadow") version "8.1.7"
+    id("maven-publish")
+}
+
+group = "com.kamikazejam"
+version = "0.0.1"
+description = "A data storage and synchronization library for Spigot plugins."
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://nexus.luxiouslabs.net/public")
+}
+
+val kamiCommonVer = "3.0.3.9"
+dependencies {
+    // Spigot (from public nexus)
+    compileOnly("net.techcable.tacospigot:server:1.8.8-R0.2-REDUCED")
+    // KamiCommon (spigot dependency)
+    compileOnly("com.kamikazejam.kamicommon:spigot-jar:$kamiCommonVer")
+
+
+
+    // Annotation Processors
+    //   Lombok
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+    testCompileOnly("org.projectlombok:lombok:1.18.32")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
+    //   JetBrains
+    compileOnly("org.jetbrains:annotations:24.1.0")
+}
+
+// We want UTF-8 for everything
+tasks.withType<JavaCompile> {
+    options.encoding = Charsets.UTF_8.name()
+}
+tasks.withType<Javadoc> {
+    options.encoding = Charsets.UTF_8.name()
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks {
+    build.get().dependsOn(shadowJar)
+
+    shadowJar {
+        minimize()
+        archiveClassifier.set("")
+        configurations = listOf(project.configurations.shadow.get())
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        val props = mapOf(
+            "name" to rootProject.name,
+            "version" to project.version,
+            "description" to project.description,
+            "date" to DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+        )
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
+
+// Publishing to Nexus
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = rootProject.group.toString()
+            artifactId = project.name
+            version = rootProject.version.toString()
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://nexus.luxiouslabs.net/private/")
+            credentials {
+                username = System.getenv("LUXIOUS_NEXUS_USER")
+                password = System.getenv("LUXIOUS_NEXUS_PASS")
+            }
+        }
+    }
+}
---
+++ src/main/java/com/kamikazejam/syncraft/SyncraftPlugin.java
@@ -0,0 +1,17 @@
+package com.kamikazejam.syncraft;
+
+import com.kamikazejam.kamicommon.KamiPlugin;
+
+@SuppressWarnings("unused")
+public class SyncraftPlugin extends KamiPlugin {
+
+    @Override
+    public void onEnableInner() {
+
+    }
+
+    @Override
+    public void onDisableInner() {
+
+    }
+}
