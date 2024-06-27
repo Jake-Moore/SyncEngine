b0a3e3dabb97c15e2c0e7528e275635e312efe31
Jake Moore <jakealexandermoore@icloud.com>
2024-06-27 02:51:34 -0700

    0.2.1 -Fix Jackson serialization of transients

--- src/main/java/com/kamikazejam/syncengine/util/JacksonUtil.java
+++ src/main/java/com/kamikazejam/syncengine/util/JacksonUtil.java
@@ -1,7 +1,9 @@
 package com.kamikazejam.syncengine.util;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,6 +15,17 @@ public class JacksonUtil {
         if (mapper != null) return mapper;
         mapper = new ObjectMapper();
         // mapper.enable(SerializationFeature.INDENT_OUTPUT);  // Optional: enable pretty printing
+
+        // Configure Jackson to only use fields for serialization (ignoring transient fields)
+        //   We have to disable setters and getters, otherwise a transient getter or setter will cause it to be serialized
+        VisibilityChecker.Std check = new VisibilityChecker.Std(
+                JsonAutoDetect.Visibility.NONE,           // don't use getters for field mapping
+                JsonAutoDetect.Visibility.NONE,           // don't use getters for field mapping
+                JsonAutoDetect.Visibility.NONE,           // don't use setters for field mapping
+                JsonAutoDetect.Visibility.NONE,           // don't use creators
+                JsonAutoDetect.Visibility.ANY             // any field
+        );
+        mapper.setVisibility(check);
 
         // Enable serialization of null and empty values
         mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
