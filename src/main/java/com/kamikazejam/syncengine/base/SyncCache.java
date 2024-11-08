d699a344f2ef5186499e5be45463d6873795df18
Jake Moore <jakealexandermoore@icloud.com>
2024-11-08 02:05:47 -0800

    0.6.0-SNAPSHOT - Config option to toggle the save warnings about no changes.

--- src/main/java/com/kamikazejam/syncengine/base/SyncCache.java
+++ src/main/java/com/kamikazejam/syncengine/base/SyncCache.java
@@ -236,8 +236,10 @@ public abstract class SyncCache<K, X extends Sync<K>> implements Comparable<Sync
             loggerService.info("Failed to save Sync " + keyToString(sync.getId()));
             return false;
         }else if (state == TriState.NOT_SET) {
-            // Log the no-changes
-            SyncFileLogger.warn(this, "No changes to save for: " + sync.getId(), trace);
+            if (EngineSource.isDebug() && EngineSource.getConfig().getBoolean("debugLogs.saveCallsWithNoWork", false)) {
+                // Log the no-changes
+                SyncFileLogger.warn(this, "No changes to save for: " + sync.getId(), trace);
+            }
         }
 
         // Push update so other servers can load the new data
